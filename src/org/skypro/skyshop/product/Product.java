7298b1a8c144358a11346c261c9c7289b895ed61
kukaracha01 <semkadarov@gmail.com>
2025-08-02 17:45:56 +0300

    что то сделано

---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,24 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+    private String name;
+    private int cost;
+
+    public Product(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return  name + ": " + cost;
+    }
+}
